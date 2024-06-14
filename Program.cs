6acdcfc5b52a3be6d3d949de6ffb4f250f423677
Douglas Batista <dobatista7@gmail.com>
2024-06-14 11:40:40 -0300

    entrada de dados campeao1,campeao2

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@
         {
             Console.WriteLine("Digite os dados do primeiro campeão: ");
             Champion champion1 = Champion.CreateChampion();
+
+            Console.WriteLine();
+            Console.WriteLine("Digite os dados do segundo campeão: ");
+            Champion champion2 = Champion.CreateChampion();
+
+            Console.WriteLine();
+            Console.WriteLine("Quantos turnos você deseja executar?");
+            int turnos = int.Parse(Console.ReadLine());
             
         }
     }
