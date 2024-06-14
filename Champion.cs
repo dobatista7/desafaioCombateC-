b140d18ac9ffa468c6184fd632892178b360d503
Douglas Batista <dobatista7@gmail.com>
2024-06-14 09:42:03 -0300

    atributos privados com auto properties e metodo construtor

--- Champion.cs
+++ Champion.cs
@@ -8,5 +8,20 @@ namespace combateCSharp
 {
     internal class Champion
     {
+        public string Name { get; private set; }
+
+        public int Life { get; private set; }
+        public int Attack { get; private set; }
+        public int Armor { get; private set; }
+        
+
+        public Champion (string name, int life, int attack, int armor)
+        {
+            Name = name;
+            Life = life;
+            Attack = attack;
+            Armor = armor;            
+        }
+
     }
 }
