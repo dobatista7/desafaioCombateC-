3fc899b935d15abd27d330ad65aa483ce551feb2
Douglas Batista <dobatista7@gmail.com>
2024-06-14 13:08:58 -0300

    entrada de dados campeao1,campeao2,turnos

--- Program.cs
+++ Program.cs
@@ -12,8 +12,27 @@
             Champion champion2 = Champion.CreateChampion();
 
             Console.WriteLine();
-            Console.WriteLine("Quantos turnos você deseja executar?");
+            Console.Write("Quantos turnos você deseja executar?  ");
             int turnos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 1; i <= turnos; i++)
+            {
+                champion1.TakeDamage(champion2.Attack);
+                champion2.TakeDamage(champion1.Attack);
+
+                Console.Write($"Resultado do turno{i}:");
+                Console.WriteLine(champion1.Status());
+                Console.WriteLine(champion2.Status());
+                Console.WriteLine();
+
+                if (champion1.Life == 0 || champion2.Life == 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("FIM DO COMBATE");
             
         }
     }
